247eb5623c7d53236072fa8f19b70cff073096fa
absicamara <absicamara@gmail.com>
2018-10-16 00:02:05 +0000

    Update config.php

--- inc/config.php
+++ inc/config.php
@@ -2,16 +2,16 @@
 
 // Define some contants
 
-define('SERVER_NAME', 'ec2-54-221-225-11.compute-1.amazonaws.com');
-define('DB_NAME', 'd7jladfk880rdv');
-define('USER_NAME', 'jimalhukeepbox');
-define('USER_PWD', '7851584f4b3a78dedf3bc47069c036763c6b9d4c9628ace7c81446810679309f');
+define('SERVER_NAME', 'jlg7sfncbhyvga14.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	');
+define('DB_NAME', 'y2v9pi0rf0z2u56o');
+define('USER_NAME', 't2e00zbnzvu26qt5');
+define('USER_PWD', 'oserwjl8z8fqn7c6');
 
 // Try connecting to MSQL
 
 try {
 	
-	$pdo = new PDO("mysql:host=".SERVER_NAME."port=5432;dbname=".DB_NAME, USER_NAME, USER_PWD);
+	$pdo = new PDO("mysql:host=".SERVER_NAME."port=3306;dbname=".DB_NAME, USER_NAME, USER_PWD);
 
 	// Set ERRMODE attribue
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
